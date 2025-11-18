a26b313c1f97fb226b153e9d79f2176b17a95a86
vitorhugoarcanjo <dias.vitorhd@gmail.com>
2025-11-17 21:14:14 -0400

    Incluindo flask + html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def appinicializar():
-    return 'Flask online'
+    return render_template('pasta_tela_inicial/paginainicial.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
